f88fb33b30b1a4c2c5c2fda6e6734f0d039ad31c
Johan Beck-Noren <johan.beck-noren@king.com>
2018-09-16 19:35:00 +0200

    Commit before gamepad refactor

---
+++ input/gamepads.lua
@@ -0,0 +1,13 @@
+
+local M = {}
+
+function M.add_gamepad(owner_id, gamepad)
+	pprint("GAMEPAD MODULE add_gamepad:")
+	--pprint(owner_id)
+	--pprint(gamepad)
+	M[owner_id] = gamepad
+	pprint(M[owner_id])
+	pprint("GAMEPAD MODULE add_gamepad END")
+end
+
+return M
