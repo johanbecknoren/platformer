ea6e63713fdd32c5eeb20833d4843d24e0d612d2
Johan Beck-Noren <johan.beck-noren@king.com>
2018-10-11 17:48:04 +0200

    Grenade play

--- main/common.lua
+++ main/common.lua
@@ -16,4 +16,27 @@ M.player_tints = player_tints
 
 M.cross_tints = cross_tints
 
+M.weapons = {}
+M.weapons[hash("m249")] = {
+	name = "KSP-90",
+	anim = "m249",
+	scale = 0.5,
+	bullets_per_second = 10,
+	magazine_size = 2000,
+	spread = 2,
+	reload_time = 1.5,
+	move_and_fire = true
+}
+
+M.weapons[hash("grenade")] = {
+	name = "HE-grenade",
+	anim = "grenade",
+	scale = 0.5,
+	bullets_per_second = 10,
+	magazine_size = 1,
+	spread = 0,
+	reload_time = 1.5,
+	move_and_fire = true
+}
+
 return M
---
+++ weapons/grenade_explosion.go
@@ -0,0 +1,30 @@
+components {
+  id: "play_particlefx"
+  component: "/weapons/play_particlefx.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "grenade"
+  component: "/effects/grenade.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
